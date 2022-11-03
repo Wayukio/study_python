09492ad349e624499036663950b5a9aabb6b0567
Wayukio <vas.yudin@gmail.com>
2022-11-03 13:27:42 +0300

    game_quess_word

--- stepik.py
+++ stepik.py
@@ -2263,17 +2263,194 @@ import random
 
 # print(caesar_ciphr())
 
-def perevod(num, system):
-    summ = 0
-    digits = [10, 11, 12, 13, 14, 15]
-    abcd = ['a', 'b', 'c', 'd', 'e', 'f']
-    for i in range(len(num)):
-        bob = num[i]
-        if bob in abcd:
-            bob = digits[abcd.index(bob)]
-        summ += int(bob) * (system ** (len(num) - 1 - i))
-    return summ
+# def perevod(num, system):
+#     summ = 0
+#     digits = [10, 11, 12, 13, 14, 15]
+#     abcd = ['a', 'b', 'c', 'd', 'e', 'f']
+#     for i in range(len(num)):
+#         bob = num[i]
+#         if bob in abcd:
+#             bob = digits[abcd.index(bob)]
+#         summ += int(bob) * (system ** (len(num) - 1 - i))
+#     return summ
 
-num = input().lower()
-system = int(input())
-print(perevod(num, system))
+# num = input().lower()
+# system = int(input())
+# print(perevod(num, system))
+
+
+# def perevod(num, system):
+#     summ = []
+#     digits = [10, 11, 12, 13, 14, 15]
+#     abcd = ['a', 'b', 'c', 'd', 'e', 'f']
+#     last = ''
+#     total = []
+#     while num >= system:
+#         summ.append(str(num % system))
+#         num //= system
+#         last = str(num)
+#     if int(last) in digits:
+#         last = abcd[digits.index(last)].upper()
+#     for i in range(len(summ)):
+#         if int(summ[i]) in digits:
+#             summ[i] = str(abcd[digits.index(int(summ[i]))].upper())
+#     total.append(last)
+#     total.extend(summ[::-1])
+#     total = ''.join(total)
+#     if total.isdigit():
+#         total = int(total)
+#     return total
+
+# num, system = int(input()), int(input())
+# print(perevod(num, system))
+
+# def perevod(num):
+#     binary = bin(num)
+#     octal = oct(num)
+#     _hex = hex(num).upper()
+#     return binary[2:], octal[2:], _hex[2:]
+
+# print(*perevod(int(input())), sep='\n')
+
+word_list = ['виселица', 'дракон', 'человек', 'пакет', 'сколопендра', 
+'парашют', 'сом', 'вася', 'соня', 'маракуйя', 'крепость', 'кинофильм', 
+'путник', 'кожура', 'перекресток', 'аорта', 'сокол', 'рефрежератор', 
+'мор', 'бобер', 'бригада', 'паста', 'кружка', 'жираф', 'лист']
+rus_al_up = [chr(ord('А') + i) for i in range(32) if chr(ord('А') + i) != 'Ё']
+rus_al_low = [chr(ord('а') + i) for i in range(32) if chr(ord('А') + i) != 'ё']
+
+def get_word():
+    word = random.choice(word_list).upper()
+    return word
+
+def display_hangman(tries):
+    stages = [
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                ''',
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                ''',
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                ''',
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                ''',
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                ''',
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                ''',
+                '''
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                '''
+    ]
+    return stages[tries]
+
+def is_true(letter, guessed_words, guessed_letters):
+    if not letter.isalpha():
+        print('Вы ввели символ, неявляющийся буквой. Попыта не засчитана, попробуйте еще раз.')
+        return True
+    if 1 < len(letter) < len(word):
+        print('Вы ввели болше одной буквы и не ввели слово целиком, думаю это ошибка, попробуйте еще раз.')
+        return True
+    if letter in guessed_letters:
+        print('Вы вводили эту букву, попробуйте еще раз.')
+        return True
+    if letter in guessed_words:
+        print('Вы вводили это слово, попробуйте еще раз.')
+        return True
+    if len(letter) == 1:
+        if letter not in rus_al_up and letter not in rus_al_low:
+            print('Буква чужого алфавита или буква "Ё". Попробуйте еще раз.')
+            return True
+
+def play_game(word):
+    word_completion = ['_'] * len(word)
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    answer = ''
+    t = 0
+    print('Давай сыграем в игру.', display_hangman(tries), word_completion, sep='\n')
+    print()
+    while True:
+        letter = input('Введите предполагаемое слово или букву: ').upper()
+        t += 1
+        if is_true(letter, guessed_words, guessed_letters):
+            continue
+        if letter not in word:
+            tries -= 1
+            if len(letter) == 1:
+                guessed_letters.append(letter)
+                print('Неверно, эта буква не подходит.')
+            else:
+                guessed_words.append(letter)
+                print('Неверно, это слово не подходит.')
+            print(display_hangman(tries), word_completion, sep='\n')
+
+        if tries == 0:
+            print(f'К сожалению, вы проиграли, вам конец. Загадано было слово: {word}.')
+            break
+        if letter in word:
+            guessed_letters.append(letter)
+            for i in range(len(word)):
+                if letter == word[i]:
+                    word_completion[i] = word[i]
+                    answer = ''.join(word_completion)
+            print(display_hangman(tries), word_completion, sep='\n')
+            if letter == word or answer.upper() == word:
+                print(f'Все верно, вы угадали слово "{word}" и победили в этой игре!', f'Всего попыток = {t}', sep='\n')
+                if guessed_words != []:
+                    print(f'Попытки введения слов: {guessed_words}')
+                if guessed_letters != []:
+                    print(f'Попытки введения букв: {guessed_letters}')
+                break
+            continue
+
+word = get_word()
+play_game(word)
